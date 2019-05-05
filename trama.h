7e36207f5e101bf9bf63ce0a023ce0487187cb74
cristian escudero <42081712+escuderoc95@users.noreply.github.com>
2019-05-04 19:15:56 -0500

    liberia de trama
    inicialización de funciones

---
+++ trama.h
@@ -0,0 +1,10 @@
+#ifndef TRAMA_H
+#define TRAMA_H
+
+using namespace std;
+
+void cipherData(int trama[20], int rondas);
+void inicializar(int *arregl_aleatorio, const int size);
+void imprimir(int *arreglo, const int size);
+#endif // TRAMA_H
+
